13cfc5ce6f2f52b5a6d777769afc1a1913356eb7
Hala <hala.tagx@gmail.com>
2023-03-17 10:16:45 -0700

    added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints the last digit of a number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 'a', z = 'z';
+
+	for (a; a <= z; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}

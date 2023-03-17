e70e6498282d302ab4b19175a12023a6f45e492b
Hala <hala.tagx@gmail.com>
2023-03-17 10:38:14 -0700

    added task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: C program that prints the lowercase letters
+ * Description: C program that prints the lowercase and uppercase letters
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char a, z = 'z';
+	char a, z = 'z', A, Z = 'Z';
 
 	for (a = 'a'; a <= z; a++)
 	{
 		putchar(a);
+	}
+	for (A = 'A'; A <= Z; A++)
+	{
+		putchar(A);
 	}
 	putchar('\n');
 	return (0);

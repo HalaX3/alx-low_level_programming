70d18d44626871ba28ca4955f507dde22b2b0abd
Hala <hala.tagx@gmail.com>
2023-03-21 21:25:29 +0000

    added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - utilizes on the _putchar function to print
+ *			all lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z' c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

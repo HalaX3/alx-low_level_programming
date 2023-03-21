6dd5f9f949cea7aaaccd05b08e29854b55953513
Hala <hala.tagx@gmail.com>
2023-03-21 20:54:30 +0000

    added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints all possible combination of two-digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+}

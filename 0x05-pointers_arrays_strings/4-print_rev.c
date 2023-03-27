538d9d3dff8114485a770895d1f12ffd24b46ac8
Hala <hala.tagx@gmail.com>
2023-03-27 14:53:37 -0700

    added all tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int x = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		x++;
+		s++;
+	}
+	s--;
+	for (i = x; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (*(src + len) != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[len] = '\0';
+	return (dest);
+}

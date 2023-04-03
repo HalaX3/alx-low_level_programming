d21bf324a3c0fcf00e7976e9cee7cd0a51afe8ca
Hala <hala.tagx@gmail.com>
2023-04-03 13:16:51 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to search.
+ * @c: The character to locate.
+ *
+ * Return: If c is found - a pointer to the first occurence of c.
+ *         If c is not found - NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+		if (s[index] == c)
+			return (s + index);
+
+	return (NULL);
+}

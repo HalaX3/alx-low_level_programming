f78d4a1e5abc54ff592b2ee64b16a06baecd8928
Hala <hala.tagx@gmail.com>
2023-04-05 19:15:18 -0700

    commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * wildcmp - takes two strings and see if they are the same
+ * @s1: string 1
+ * @s2: string 2 which may contain wildcard *
+ *
+ * Return: 1 if the strings are identical, otherwise return 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		else
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+
+	if (*s1 == '\0')
+	{
+		if (*s2 == '\0' || *s2 == '*')
+			return (1);
+		else
+			return (0);
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else
+		return (0);
+}

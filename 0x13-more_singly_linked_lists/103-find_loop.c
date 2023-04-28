aab9e488303c415bafbeadd3d4cc7de2be876928
Hala <hala.tagx@gmail.com>
2023-04-28 00:49:18 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - function that finds the loop in LL
+ * @head: LL to be searched for
+ *
+ * Return: address of node, NULL otherwise
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *s = head;
+	listint_t *f = head;
+
+	if (!head)
+		return (NULL);
+
+	while (f && s && f->next)
+	{
+		s = s->next;
+		f = f->next->next;
+		if (f == slow)
+		{
+			s = head;
+			while (f != s)
+			{
+				f = f->next;
+				s = s->next;
+			}
+			return (f);
+		}
+	}
+
+	return (NULL);
+}
